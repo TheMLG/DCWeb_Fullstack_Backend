f137d9923ec52a81c092572d13a9f551a3ef022f
TheMLG <mlgoswami1801@gmail.com>
2025-06-18 12:33:07 +0530

    Add node-cron dependency and implement scheduled task in server

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ dotenv.config({
 });
 
 
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +22,11 @@ app.get('/', (_req, res) => {
     res.json({ message: 'API is running' });
 });
 
+import cron from 'node-cron';
+
+cron.schedule('* * * * *', () => {
+  console.log('running a task every minute');
+});
 
 import reviewRoute from "./routes/reviewRoute.js"
 import contributeRoute from "./routes/contributeRoute.js";
